ffde2d06e39964c7469c5b51be761ef71d426c6d
Ciprian Danis <cdanis39@gmail.com>
2020-10-15 11:03:17 +0300

    Added IMR lab2 assignment files

---
+++ Lab2/Assets/Scripts/ChalkLineRenderer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using VRTK;
+using Unity;
+
+public class ChalkLineRenderer : MonoBehaviour
+{ 
+    private LineRenderer lineRenderer;
+    public float minimumVertexDistance = 0.05f;
+    private Transform chalkTip;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        chalkTip = gameObject.transform.GetChild(0);
+    }
+
+    void Update()
+    {
+        
+        float distance = Vector3.Distance(chalkTip.position,
+            lineRenderer.GetPosition((lineRenderer.positionCount - 1)));
+        if (distance > minimumVertexDistance)
+        {
+            Debug.Log(distance);
+            UpdateLine();
+        }
+
+    }
+
+    private void onEnable()
+    {
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, chalkTip.position);
+        lineRenderer.SetPosition(1, chalkTip.position);
+    }
+
+    private void UpdateLine()
+    {
+        lineRenderer.positionCount++;
+        lineRenderer.SetPosition(lineRenderer.positionCount - 1, chalkTip.position);
+    }
+}
